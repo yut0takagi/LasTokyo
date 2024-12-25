c5cb303e0055aa49a6e6e2e50ba1a5053a2f21c9
yut0takagi <yutotkg.1040@gmail.com>
2024-12-25 23:46:41 +0900

    add

--- las.py
+++ las.py
@@ -1,1 +1,15 @@
 #lasデータを編集、解析するためのファイルです
+import laspy
+import pyarrow
+
+
+def GetCoodinate(filepath):
+    x_first = las.X[0]
+    x_scale = header.x_scale
+    x_offset = header.x_offset
+    x_coordinate = (x_first * x_scale) + x_offset
+
+    print(f"{x_first=}")
+    print(f"{x_scale=}")
+    print(f"{x_offset=}")
+    print(f"{x_coordinate=}")
