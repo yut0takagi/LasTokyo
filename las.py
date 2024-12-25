0b006748f51d1acb3371dee4785060901ad50791
yut0takagi <yutotkg.1040@gmail.com>
2024-12-26 00:54:43 +0900

    now changed

--- las.py
+++ las.py
@@ -1,15 +1,29 @@
 #lasデータを編集、解析するためのファイルです
 import laspy
-import pyarrow
+import pyarrow as pa
+import pyarrow.parquet as pq
 
-
-def GetCoodinate(filepath):
-    x_first = las.X[0]
-    x_scale = header.x_scale
-    x_offset = header.x_offset
-    x_coordinate = (x_first * x_scale) + x_offset
-
-    print(f"{x_first=}")
-    print(f"{x_scale=}")
-    print(f"{x_offset=}")
-    print(f"{x_coordinate=}")
+class LasTokyo:
+    def __init__(self,filepath):
+        self.filepath = filepath
+        self.las = laspy.read(self.filepath)
+        self.header = self.las.header
+        self.version_major = self.las.header.version.major
+        self.version_minor = self.las.header.version.minor
+    def getVersion(self):
+        version = f"{self.version_major}.{self.version_minor}"
+        return version
+    def las_array(self):
+        X = self.las.X
+        Y = self.las.Y
+        Z = self.las.Z
+        return [X,Y,Z]
+    def getCoordinates(self):
+        data = self.las_array()
+        table = pa.Table.from_arrays(
+            [pa.array(data[0]),
+             pa.array(data[1]),
+             pa.array(data[2])],
+            names=["x","y","z"]
+        )
+        print(table)
